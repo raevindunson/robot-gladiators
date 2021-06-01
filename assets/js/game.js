95f53b24d7bed3157ef6c586ccef22f0bbf50614
Raevin Dunson <raevin.dunson98@gmail.com>
2021-06-01 17:54:18 -0400

    initial js files

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,23 @@
+var playerName = window.prompt("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+
 window.alert("This is an alert! JavaScript is running!");
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+  fight ();
+
+// This is a String data type; it must be wrapped in double quotes (" ") or single quotes (' ').
+var stringDataType = "This is a string, which is a fancy way to say text";
+
+// This is a Number data type; it can be an integer (whole number) or have decimals (floated numbers).
+var numberIntegerDataType = 10;
+var numberFloatDataType = 10.4;
+
+// This is a Boolean data type, which can only be given a value of true or false.
+var booleanDataType = true;
