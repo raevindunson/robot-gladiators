3e24680747ed15e1731fe18a9f0fd77e6d194b6b
Raevin Dunson <raevin.dunson98@gmail.com>
2021-06-03 17:12:39 -0400

    manage data with functions variables and operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,43 +1,33 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-var name = "your name";
-console.log(name)
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// Tony the Robot is ready for battle!
-console.log("Tony the Robot" + " is ready for battle!");
+var fight = function() {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
 
-// "Tony the Robot is ready for battle!"
-console.log(playerName + " is ready for battle!");
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth = enemyHealth - playerAttack;
 
-// "Your robot, Tony the Robot, has won!
-console.log("Your robot, " + playerName + ", has won!");
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+  
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemyAttack;
 
-window.alert("This is an alert! JavaScript is running!");
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+};
 
-// this creates a function named "fight"
-
-function fight() {
-    window.alert("The fight has begun!");
-  }
-
-  fight ();
-
-// This is a String data type; it must be wrapped in double quotes (" ") or single quotes (' ').
-var stringDataType = "This is a string, which is a fancy way to say text";
-
-// This is a Number data type; it can be an integer (whole number) or have decimals (floated numbers).
-var numberIntegerDataType = 10;
-var numberFloatDataType = 10.4;
-
-// This is a Boolean data type, which can only be given a value of true or false.
-var booleanDataType = true;
-
-
+fight();
