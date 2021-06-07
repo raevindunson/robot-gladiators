dd472c481abb9d5cbd8259c46716843427e0cc03
Raevin Dunson <raevin.dunson98@gmail.com>
2021-06-06 22:19:18 -0400

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -25,18 +25,70 @@ var startGame = function() {
     playerHealth = 100;
     playerAttack = 10;
     playerMoney = 10;
+
+    fight(pickedEnemyName);
+
+    // if we're not at the last enemy in the array
+    // if player is still alive and we're not at the last enemy in the array
+if (playerHealth > 0 && i < enemyNames.length - 1) {
+    // ask if player wants to use the store before next round
+    var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+  
+    // if yes, take them to the store() function
+    if (storeConfirm) {
+      shop();
+    }
+    var shop = function() {
+      // ask player what they'd like to do
+      var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+      );
+    };
+    // use switch to carry out action
+    switch (shopOptionPrompt) {
+      case "REFILL": // new case
+      case "refill":
+        if (playerMoney >= 7) {
+          window.alert("Refilling player's health by 20 for 7 dollars.");
+    
+          playerHealth = playerHealth + 20;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+    
+        break;
+      case "UPGRADE": // new case
+      case "upgrade":
+        if (playerMoney >= 7) {
+          window.alert("Upgrading player's attack by 6 for 7 dollars.");
+    
+          playerAttack = playerAttack + 6;
+          playerMoney = playerMoney - 7;
+        }
+        else {
+          window.alert("You don't have enough money!");
+        }
+    
+        break;
+      case "LEAVE": // new case
+      case "leave":
+        window.alert("Leaving the store.");
+        break;
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+        shop();
+        break;
+    }
+}
+    }
   
     // other logic remains the same...
   };
-  
-  var endGame = function() {
-    // if player is still alive, player wins!
-    if (playerHealth > 0) {
-      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
-    } 
-    else {
-      window.alert("You've lost your robot in battle.");
-    }
+
+
+
     // ask player if they'd like to play again
 var playAgainConfirm = window.confirm("Would you like to play again?");
 
@@ -158,6 +210,21 @@ for (var i = 0; i < enemyNames.length; i++) {
         break;
       }
     }
+var endGame = function() {
+      // if player is still alive, player wins!
+      if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+      } 
+      else {
+        window.alert("You've lost your robot in battle.");
+      }
+  
+      var shop = function() {
+        console.log("entered the shop");
+      };
+
+
+
     // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
   endGame();
   };
